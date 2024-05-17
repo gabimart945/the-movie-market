2f75ebfdcde23a5d9739bd89ca9f18e66ae48a55
Gabriel Martinez <gabriel.martinez945@comunidadunir.net>
2024-05-17 09:00:11 +0200

    some class names to BEM

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,13 +1,16 @@
 import React from "react";
 import '../styles/Rating.css'
+
+
+// Adapted from https://webdesign.tutsplus.com/a-simple-javascript-technique-for-filling-star-ratings--cms-29450t
 export const Rating = ({rating, className}) => {
 
     const percentage = String(rating * 100 / 5) + '%';
 
     return (
         <div className={className}>
-            <div className="stars-outer">
-                <div className="stars-inner" style={{width: percentage}}></div>
+            <div className="rating__stars">
+                <div className="stars__inner" style={{width: percentage}}></div>
             </div>
             <span className='rating__span'> ({rating}/5.0)</span>
         </div>
